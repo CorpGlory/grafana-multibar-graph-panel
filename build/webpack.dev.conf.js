9bdef802e14b28ea44b7c97e6dddc68f43966e58
rozetko <rozetko@corpglory.com>
2018-10-04 20:29:21 +0300

    source-map fixes

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,8 +1,14 @@
+const webpack = require('webpack');
 const baseWebpackConfig = require('./webpack.base.conf');
 
 var conf = baseWebpackConfig;
 conf.watch = true;
-conf.devtool = 'source-map';
+conf.devtool = 'inline-source-map';
 conf.mode = 'development';
+conf.plugins.push(new webpack.SourceMapDevToolPlugin({
+  filename: null,
+  exclude: [/node_modules/],
+  test: /\.ts$/
+}));
 
 module.exports = conf;
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -3,7 +3,6 @@ const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
 var conf = baseWebpackConfig;
 conf.mode = 'production';
-conf.devtool = 'source-map';
 
 conf.plugins.push(new ngAnnotatePlugin());
 
