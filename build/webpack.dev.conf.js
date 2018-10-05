5c33f622e324185c1d3472e2331dab3dfd2e5513
rozetko <rozetko@corpglory.com>
2018-10-05 14:44:20 +0300

    fix webpack dev config

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -3,12 +3,7 @@ const baseWebpackConfig = require('./webpack.base.conf');
 
 var conf = baseWebpackConfig;
 conf.watch = true;
-conf.devtool = 'inline-source-map';
+conf.devtool = 'source-map';
 conf.mode = 'development';
-conf.plugins.push(new webpack.SourceMapDevToolPlugin({
-  filename: null,
-  exclude: [/node_modules/],
-  test: /\.ts$/
-}));
 
 module.exports = conf;
